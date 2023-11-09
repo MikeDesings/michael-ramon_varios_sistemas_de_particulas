2f3649b1575c87be3613743daa7c0fa1e8c8ab33
Michael Ramón <michael.ramon@ucuenca.edu.ec>
2023-11-09 15:09:45 -0500

    Updates
    Tercera Parte del Código

--- classes/Particula.js
+++ classes/Particula.js
@@ -7,6 +7,23 @@ class Particula {
   }
   update() {
     this.r = random();
+
+    if (this.r < 0.25) {
+        // hacia arriba
+      this.pos.x -= this.side;
+    } else if (this.r < 0.5) {
+        // hacia la derecha
+      this.pos.x += this.side;
+    } else if (this.r < 0.75) {
+        // hacia abajo
+        this.pos.x += this.side;
+    } else (this.r < 0.6) {
+        // hacia la izquierda
+    }
+    this.lifespan -= 1;
+    if(this.lifespan <= 0){
+        this.isAlive = false;
+    }
   }
   display() {
     square(this.pos.x, this.pos.y, this.side);
